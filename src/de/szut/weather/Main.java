4208ec88aa05c4d8945ca367655e8d636f8f06e1
nunull <timmm.albers@gmail.com>
2014-02-10 14:26:34 +0100

    Added some GUI-wrappers.

--- src/de/szut/weather/Main.java
+++ src/de/szut/weather/Main.java
@@ -3,10 +3,11 @@ package de.szut.weather;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.ObjectInputStream.GetField;
 import java.util.LinkedList;
-import java.util.TreeMap;
 
+import javax.swing.SwingUtilities;
+
+import de.szut.weather.gui.MainWindow;
 import de.szut.weather.models.Entry;
 import de.szut.weather.parser.CSVReader;
 import de.szut.weather.stats.*;
@@ -17,21 +18,21 @@ public class Main {
 	 * @param args ignored
 	 */
 	public static void main(String[] args) {
-		
-		/* testing parser */
-		CSVReader reader = new CSVReader();
-		
-		try {
-			LinkedList<Entry> entrys = reader.read(new BufferedReader(new FileReader(Config.CSV_PATH)));
-			WeatherStats stats = new WeatherStats(entrys);
-			System.out.println( stats.getTm() );
-			System.out.println( stats.getShk().getValueAsString("SHK") );
-			System.out.println( stats.getFx().get(3).getValueAsString("FX"));
-			System.out.println( stats.getTx().get(4).getValueAsDouble("TX"));
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+		SwingUtilities.invokeLater(new Runnable() {
 
+			@Override
+			public void run() {
+				try {
+					CSVReader reader = new CSVReader();
+					LinkedList<Entry> entrys = reader.read(new BufferedReader(new FileReader(Config.CSV_PATH)));
+					WeatherStats stats = new WeatherStats(entrys);
+
+					MainWindow mainW = new MainWindow(stats);
+					mainW.show();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
