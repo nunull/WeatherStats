3fa74cbc4ae793b94b2a3e93b0cd4f132b45fd2b
Timm Albers <timmm.albers@gmail.com>
2014-03-04 11:47:54 +0100

    Update Config.java

--- src/de/szut/weather/Config.java
+++ src/de/szut/weather/Config.java
@@ -1,5 +1,5 @@
 package de.szut.weather;
 
 public final class Config {
-	public final static String CSVPATH = "trunk/ressources/Klima_Tageswerte_1890_2013.csv";
+	public final static String CSVPATH = "your/path/to/Klima_Tageswerte_1890_2013.csv";
 }
