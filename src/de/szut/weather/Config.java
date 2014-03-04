838ff8cc4815b5241d8432cc0b9ebda06536eab4
nunull <timmm.albers@gmail.com>
2014-03-04 11:48:55 +0100

    Updated class path

--- src/de/szut/weather/Config.java
+++ src/de/szut/weather/Config.java
@@ -1,5 +1,5 @@
 package de.szut.weather;
 
 public final class Config {
-	public final static String CSVPATH = "trunk/ressources/Klima_Tageswerte_1890_2013.csv";
+	public final static String CSVPATH = "ressources/Klima_Tageswerte_1890_2013.csv";
 }
